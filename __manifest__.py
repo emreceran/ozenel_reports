06a9941c62ba48a828e162bceaf307fede1f34c7
emreceran <emrecrn@gmail.com>
2025-08-01 10:51:05 +0300

    apa

--- __manifest__.py
+++ __manifest__.py
@@ -3,12 +3,24 @@
     'version': '1.0',
     'summary': 'Kapalı Teklif raporu: sadece toplam tutarı gösterir',
     'author': 'Emre',
-    'depends': ['sale'],
+    'depends': ['sale', 'web', 'sale_management'],
     'data': [
-        'report/sale_report.xml',
+        'report/paperformat.xml',
         'report/sale_report_templates.xml',
         'views/sale_order_line_views.xml',
+        'report/sale_report.xml',
+        'report/kapali.xml',
+        'report/report_kapali.xml',
+
+
     ],
+
+    'assets': {
+        'web.report_assets_common': [
+            'ozenel_reports/static/src/css/report_styles.css',
+        ],
+    },
+
     'installable': True,
     'application': False,
 }
