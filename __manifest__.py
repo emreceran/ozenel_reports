704bf375fc7f31dd38e8b01dd409485e54da8cb6
emreceran <emrecrn@gmail.com>
2025-08-05 21:06:24 +0300

    tüm pdfler

--- __manifest__.py
+++ __manifest__.py
@@ -5,13 +5,15 @@
     'author': 'Emre',
     'depends': ['sale', 'web', 'sale_management'],
     'data': [
-        'report/paperformat.xml',
-        'report/sale_report_templates.xml',
+        # 'report/paperformat.xml',
+        # 'report/sale_report_templates.xml',
         'views/sale_order_line_views.xml',
-        'report/sale_report.xml',
-        'report/kapali.xml',
+        # 'report/sale_report.xml',
+        # 'report/kapali.xml',
         'report/report_cover_page.xml',
         'report/report_sale_order_body.xml',
+        'report/report_sale_order_body_closed.xml',
+        'report/report_sale_order_body_subtotal.xml',
         'report/sale_report_views.xml',
 
 
