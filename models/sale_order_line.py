b4d1e9be9546a985b66ccaccdb25b555bb9be2c7
emreceran <emrecrn@gmail.com>
2025-08-12 09:49:47 +0300

    icmal

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -18,3 +18,40 @@ class SaleOrderLine(models.Model):
                 lambda attr: attr.attribute_id.name.lower() == 'marka'
             )
             line.x_brand_name = brand_value.name if brand_value else ''
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    # _get_lines_for_report_grouped fonksiyonu isteğiniz üzerine kaldırıldı.
+
+    def _get_section_subtotals(self):
+        """
+        Kapak sayfasındaki 'İcmal' tablosu için her bölümün ara toplamını hesaplar.
+        """
+        self.ensure_one()
+        section_totals = []
+        current_section = None
+        current_subtotal = 0.0
+
+        lines_to_report = self._get_order_lines_to_report()
+
+        for line in lines_to_report:
+            if line.display_type == 'line_section':
+                if current_section:
+                    section_totals.append({
+                        'name': current_section.name,
+                        'subtotal': current_subtotal,
+                    })
+                current_section = line
+                current_subtotal = 0.0
+            elif not line.display_type and current_section:
+                current_subtotal += line.price_subtotal
+
+        if current_section:
+            section_totals.append({
+                'name': current_section.name,
+                'subtotal': current_subtotal,
+            })
+
+        return section_totals
